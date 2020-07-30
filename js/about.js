ac3e1438087816ba98f2d902d3d4b6d12d63b405
Ronald Cheng <ronaldcheng11@gmail.com>
2020-07-29 22:21:37 -0400

    About page change

---
+++ js/about.js
@@ -0,0 +1,8 @@
+
+$(".gallery-slot").hover(function(){
+  let id = $(this).attr("id").split("-")[2]
+  $("#gallery-text-"+id).show()
+}, function(){
+  let id = $(this).attr("id").split("-")[2]
+  $("#gallery-text-"+id).hide()
+})
